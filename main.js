4797274e3d95617b50116d8b995a05886e668db5
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-01-26 23:04:20 +0100

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const input = 'whale talk';
 const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
-for ( let inputIndex = 0; inputIndex<input.length; inputIndex++) {
-  console.log(`inputIndex is ${inputIndex}`);
+for ( let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+  //console.log(`inputIndex is ${inputIndex}`);
+  for (let vowelsindex = 0 ; vowelsindex<vowels.length;vowelsindex++) {console.log(`vowelsindex is ${vowelsindex}`)}
 }
+
+//console.log(`vowelsindex is ${vowelsindex}`)
