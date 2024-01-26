ade5e5d687985d45d294869a2aad55f0f3e44584
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-01-27 00:21:13 +0100

    main.js

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
 for ( let inputIndex = 0; inputIndex < input.length; inputIndex++) {
   //console.log(`inputIndex is ${inputIndex}`);
-  for (let vowelsindex = 0 ; vowelsindex<vowels.length;vowelsindex++) {console.log(`vowelsindex is ${vowelsindex}`)}
-}
+  for (let vowelsindex = 0 ; vowelsindex<vowels.length;vowelsindex++) //{console.log(`vowelsindex is ${vowelsindex}`)
+  if (input[inputIndex] === vowels[vowelsindex]) {
+    console.log(input[inputIndex])
+    resultArray.push(input[inputIndex]);
+  }
+  }
+
 
 //console.log(`vowelsindex is ${vowelsindex}`)
