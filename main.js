7032feda54c034dff38f4ba6332a13619dd068c8
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-01-26 19:05:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const input = 'whale talk';
+const vowels = ["a", "e", "i", "o", "u"];
+const resultArray = [];
+for ( let inputIndex = 0; inputIndex<input.length; inputIndex++) {
+  console.log(`inputIndex is ${inputIndex}`);
+}
