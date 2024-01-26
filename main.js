5e7a40c185d7dfed9fa20c37f69aed271d5b054a
LOHAMER <130869756+LO-HAMER@users.noreply.github.com>
2024-01-27 00:39:42 +0100

    main.js

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const input = 'whale talk';
 const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
-for ( let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+for ( let inputIndex = 0; inputIndex < input.length; inputIndex++) { 
+  if (input[inputIndex] === 'e'){
+    resultArray.push(input[inputIndex]);
+  } if (input[inputIndex] === 'u'){
+    resultArray.push(input[inputIndex]);
+  }
   //console.log(`inputIndex is ${inputIndex}`);
   for (let vowelsindex = 0 ; vowelsindex<vowels.length;vowelsindex++) //{console.log(`vowelsindex is ${vowelsindex}`)
   if (input[inputIndex] === vowels[vowelsindex]) {
-    console.log(input[inputIndex])
+    //console.log(input[inputIndex])
     resultArray.push(input[inputIndex]);
   }
   }
 
+const resultString = resultArray.join('').toUpperCase();
+console.log(resultString)
 
-//console.log(`vowelsindex is ${vowelsindex}`)
